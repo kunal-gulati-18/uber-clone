4f1a085a7a2ecb1ad06ac3cabc4597c7f418a9d9
kunalGEntytle <kunal.gulati@entytle.com>
2022-05-08 19:45:54 +0530

    second commit

--- uber-next/pages/components/ActionItems.js
+++ uber-next/pages/components/ActionItems.js
@@ -1,9 +1,67 @@
-import React from 'react'
+import React from 'react';
+import tw from 'tailwind-styled-components';
+
+const ActionItemsWrapper = tw.div `
+  flex-1 px-5
+`
+
+const ActionItemHeader = tw.div `
+	flex justify-between items-center
+`;
+
+const ActionItemHeading = tw.img `
+h-10
+`;
+
+const ActionItemProfileSection = tw.div `
+flex justify-between items-center gap-x-4
+`;
+
+const ActionItemProfileName = tw.span `
+text-xs font-semibold
+`;
+
+const ActionItemProfilePicture = tw.span ``;
+
+const ActionItemsButtons = tw.div `
+flex gap-x-2 
+`;
+
+const ActionItemsFooter = tw.div ``;
 
 const ActionItems = () => {
-  return (
-    <div>ActionItems</div>
-  )
+    return (
+        <ActionItemsWrapper className = "flex flex-col gap-y-6 ">
+            <ActionItemHeader>
+                <ActionItemHeading src = "/assets/uberLogo.png"/>
+                <ActionItemProfileSection>
+                    <ActionItemProfileName>
+                        Kunal Gulati
+                    </ActionItemProfileName>
+                    <ActionItemProfilePicture>
+                        <img class="inline-block h-8 w-8 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
+                    </ActionItemProfilePicture>
+                </ActionItemProfileSection>
+            </ActionItemHeader>
+            <ActionItemsButtons>
+                <button className='bg-zinc-100 flex flex-col flex-1 h-24 items-center justify-center rounded'>
+                    <span></span>
+                    <span className='buttonText'>Ride</span>
+                </button>
+                <button className='bg-zinc-100 flex flex-col flex-1 h-24 items-center justify-center rounded'>
+                    <span></span>
+                    <span className='buttonText'>2-Wheels</span>
+                </button>
+                <button className='bg-zinc-100 flex flex-col flex-1 h-24 items-center justify-center rounded'>
+                    <span></span>
+                    <span className='buttonText'>Reserve</span>
+                </button>
+            </ActionItemsButtons>
+			<ActionItemsFooter className = "bg-zinc-100 p-16">
+				Where to?
+			</ActionItemsFooter>
+        </ActionItemsWrapper>
+    )
 }
 
 export default ActionItems
--- uber-next/pages/components/Map.js
+++ uber-next/pages/components/Map.js
@@ -1,27 +1,37 @@
 import React, {useEffect, useRef, useState} from 'react'
+import mapboxgl from 'mapbox-gl';
+import tw from 'tailwind-styled-components';
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoia3VuYWxndWxhdGkwODE4IiwiYSI6ImNsMngyZWlxNjBybHkza2xibjJpY3NrbjEifQ.jph0u9y6XM4ySmoi0J-eGQ';
+
+const MapContainer = tw.div `
+  flex-1
+`;
 
 const Map = () => {
-    const map = useRef(null);
-    const mapContainer = useRef();
-    const [lng, setLng] = useState(77.2610);
-    const [lat, setLat] = useState(28.5354);
-    const [zoom, setZoom] = useState(9);
-    useEffect(() => {
-        if (map.current) 
-            return;
-         // initialize map only once
-        map.current = new mapboxgl.Map({
-            container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/dark-v10',
-            center: [
-                lng, lat
-            ],
-            zoom: zoom
-        });
-    }, []);
-    return (
-        <div>Map</div>
-    )
+	const map = useRef(null);
+	const mapContainer = useRef();
+	const [lng, setLng] = useState(77.2610);
+	const [lat, setLat] = useState(28.5354);
+	const [zoom, setZoom] = useState(9);
+	useEffect(() => {
+		if (map.current) 
+			return;
+		
+		// initialize map only once
+		map.current = new mapboxgl.Map({
+			container: mapContainer.current,
+			style: 'mapbox://styles/mapbox/dark-v10',
+			center: [
+				lng, lat
+			],
+			zoom: zoom
+		});
+	}, []);
+	return (
+		<MapContainer ref={mapContainer}
+			id="map-container">Map</MapContainer>
+	)
 }
 
 export default Map;
