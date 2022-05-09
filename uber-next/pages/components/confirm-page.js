bc2aff48244bd077435d166a453fa7fe18de1a68
KunalGulati <kunal@thelevel.ai>
2022-05-09 17:35:25 +0530

    fourth commit

--- uber-next/pages/components/confirm-page.js
+++ uber-next/pages/components/confirm-page.js
@@ -1,11 +1,90 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react';
+import tw from 'tailwind-styled-components';
+import { useRouter } from 'next/router';
+import mapboxgl from 'mapbox-gl';
+import Link from 'next/link';
+import axios from 'axios';
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoia3VuYWxndWxhdGkwODE4IiwiYSI6ImNsMngyZWlxNjBybHkza2xibjJpY3NrbjEifQ.jph0u9y6XM4ySmoi0J-eGQ';
 
 const ConfirmPage = () => {
+    const map = useRef();
+    const mapContainer = useRef(null);
+    const router = useRouter();
+    const [lng, setLng] = useState(77.2610);
+	const [lat, setLat] = useState(28.5354);
+	const [zoom, setZoom] = useState(9);
+
+    useEffect(() => {
+        const pickup = router?.query?.pickup;
+        const destination = router?.query?.destination;
+
+        if(!pickup || !destination) {
+            return
+        }
+        const fetchData = async () => {
+            let data = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${pickup};${destination}.json?access_token=pk.eyJ1Ijoia3VuYWxndWxhdGkwODE4IiwiYSI6ImNsMngyZWlxNjBybHkza2xibjJpY3NrbjEifQ.jph0u9y6XM4ySmoi0J-eGQ`)
+            return data;
+        }
+
+        const data = fetchData();
+
+        // console.log('data', data)
+        // return
+        if(map.current) return;
+        map.current = new mapboxgl.Map({
+			container: mapContainer.current,
+			style: 'mapbox://styles/mapbox/dark-v10',
+			center: [
+				lng, lat
+			],
+			zoom: zoom
+		});
+    }, [router.query]);
     return (
-        <div>
-            
-        </div>
+        <ConfirmPageWrapper>
+            <MapWrapper ref = {mapContainer} id="confirm-page-map-wrapper">
+                Hello
+            </MapWrapper>
+            <ShowRideWrapper>
+                <MessageWrapper>
+                    Choose a ride, or swipe up for more
+                </MessageWrapper>
+            </ShowRideWrapper>
+            <FooterWrapper>
+                <ConfirmButton>
+                    Confirm uber
+                </ConfirmButton>
+            </FooterWrapper>
+        </ConfirmPageWrapper>
     )
 }
+
+const ConfirmPageWrapper = tw.div`
+    h-screen flex flex-col
+`;
+
+const MapWrapper = tw.div`
+    flex-1
+
+`;
+
+const ShowRideWrapper = tw.div`
+    flex-1
+`;
+
+const MessageWrapper = tw.div`
+    border-b-2 border-slate-200 flex items-center justify-center text-sm text-slate-500
+`;
+
+const CarOptionsWrapper = tw.div``
+
+const FooterWrapper = tw.div`
+p-2.5 bg-white border-t-2 border-slate-200
+`;
+
+const ConfirmButton = tw.button`
+bg-black text-white text-xl w-full p-3 flex items-center justify-center
+`;
 
 export default ConfirmPage
--- uber-next/pages/components/search-location.js
+++ uber-next/pages/components/search-location.js
@@ -1,87 +1,120 @@
 import React, { useState } from "react";
 import tw from "tailwind-styled-components";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const SearchLocation = () => {
+  const router = useRouter();
   const [fields, setFields] = useState({
     pickup: "",
     destination: "",
   });
+
+  const [showError, setShowError] = useState(false);
 
   const onChange = (e) => {
     setFields({ ...fields, [e.target.name]: e.target.value });
   };
 
   const handleClick = () => {
-    if(!pickup || !destination) {
-
+    if (!fields.pickup || !fields.destination) {
+      setShowError(true);
+      return setTimeout(() => {
+        setShowError(false);
+      }, 2000);
     }
 
-    
-  }
+    router.push(`/components/confirm-page?pickup=${fields.pickup}&destination=${fields.destination}`);
+  };
 
   return (
-    <SearchWrapper>
-      <SearchHeaderWrapper>
-        <Link href="/">
-          <BackButton>
-            <img
-              className="h-9 w-9"
-              src="https://img.icons8.com/ios-filled/50/000000/left.png"
-            />
-          </BackButton>
-        </Link>
+    <>
+      {showError ? (
+        <div
+          className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+          role="alert"
+        >
+          <span className="block sm:inline">Please enter the required fields.</span>
+          <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+            <svg
+              className="fill-current h-6 w-6 text-red-500"
+              role="button"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <title>Close</title>
+              <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+            </svg>
+          </span>
+        </div>
+      ) : null}
+      <SearchWrapper>
+        <SearchHeaderWrapper>
+          <Link href="/">
+            <BackButton>
+              <img
+                className="h-9 w-9"
+                src="https://img.icons8.com/ios-filled/50/000000/left.png"
+              />
+            </BackButton>
+          </Link>
 
-        <FieldWrapper>
-          <FieldMarkers className="flex flex-col">
-            <img
-              className="h-3 w-3"
-              src="https://img.icons8.com/ios-filled/50/9CA3AF/filled-circle.png"
-            />
-            <img src="https://img.icons8.com/ios/50/9CA3AF/vertical-line.png" />
-            <img
-              className="h-3.5 w-3.5"
-              src="https://img.icons8.com/windows/50/000000/square-full.png"
-            />
-          </FieldMarkers>
-          <FieldInputs className="flex flex-col">
-            <FieldInput
-              type="text"
-              value={fields.pickup}
-              onChange={onChange}
-              name="pickup"
-              placeholder="Enter pickup location"
-            />
-            <FieldInput
-              type="text"
-              onChange={onChange}
-              value={fields.destination}
-              name="destination"
-              placeholder="Where to?"
-            />
-          </FieldInputs>
-          <FieldAddButtonWrapper>
-            <FieldAddButton>
-                <img className = "w-5 h-5" src="https://img.icons8.com/ios/50/000000/plus-math.png" />
-            </FieldAddButton>
-          </FieldAddButtonWrapper>
-        </FieldWrapper>
-      </SearchHeaderWrapper>
-      <SearchBodyWrapper>
-        <SavedPlacesWrapper>
-          <SavedPlacesImage>
-            <img
-              className="h-8 w-8 p-1"
-              src="https://img.icons8.com/ios-filled/50/ffffff/star--v1.png"
-            />
-          </SavedPlacesImage>
-          <span>Saved Places</span>
-        </SavedPlacesWrapper>
-        <ConfirmLocationButtonWrapper>
-          <ConfirmLocationButton onClick = {handleClick}>Confirm Locations</ConfirmLocationButton>
-        </ConfirmLocationButtonWrapper>
-      </SearchBodyWrapper>
-    </SearchWrapper>
+          <FieldWrapper>
+            <FieldMarkers className="flex flex-col">
+              <img
+                className="h-3 w-3"
+                src="https://img.icons8.com/ios-filled/50/9CA3AF/filled-circle.png"
+              />
+              <img src="https://img.icons8.com/ios/50/9CA3AF/vertical-line.png" />
+              <img
+                className="h-3.5 w-3.5"
+                src="https://img.icons8.com/windows/50/000000/square-full.png"
+              />
+            </FieldMarkers>
+            <FieldInputs className="flex flex-col">
+              <FieldInput
+                type="text"
+                value={fields.pickup}
+                onChange={onChange}
+                name="pickup"
+                placeholder="Enter pickup location"
+              />
+              <FieldInput
+                type="text"
+                onChange={onChange}
+                value={fields.destination}
+                name="destination"
+                placeholder="Where to?"
+              />
+            </FieldInputs>
+            <FieldAddButtonWrapper>
+              <FieldAddButton>
+                <img
+                  className="w-5 h-5"
+                  src="https://img.icons8.com/ios/50/000000/plus-math.png"
+                />
+              </FieldAddButton>
+            </FieldAddButtonWrapper>
+          </FieldWrapper>
+        </SearchHeaderWrapper>
+        <SearchBodyWrapper>
+          <SavedPlacesWrapper>
+            <SavedPlacesImage>
+              <img
+                className="h-8 w-8 p-1"
+                src="https://img.icons8.com/ios-filled/50/ffffff/star--v1.png"
+              />
+            </SavedPlacesImage>
+            <span>Saved Places</span>
+          </SavedPlacesWrapper>
+          <ConfirmLocationButtonWrapper>
+            <ConfirmLocationButton onClick={handleClick}>
+              Confirm Locations
+            </ConfirmLocationButton>
+          </ConfirmLocationButtonWrapper>
+        </SearchBodyWrapper>
+      </SearchWrapper>
+    </>
   );
 };
 
