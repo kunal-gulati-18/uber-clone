eca39f1112af26d0ec861a4cd6be4ae9e6ec3b07
KunalGulati <kunal@thelevel.ai>
2022-05-11 17:10:17 +0530

    fourth commit

--- uber-next/pages/components/confirm-page.js
+++ uber-next/pages/components/confirm-page.js
@@ -1,64 +1,89 @@
-import React, { useEffect, useRef, useState } from 'react';
-import tw from 'tailwind-styled-components';
-import { useRouter } from 'next/router';
-import mapboxgl from 'mapbox-gl';
-import Link from 'next/link';
-import axios from 'axios';
+import React, { useEffect, useRef, useState } from "react";
+import tw from "tailwind-styled-components";
+import { useRouter } from "next/router";
+import mapboxgl from "mapbox-gl";
+import Link from "next/link";
+import axios from "axios";
+import { carList } from "../../public/assets/carList";
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoia3VuYWxndWxhdGkwODE4IiwiYSI6ImNsMngyZWlxNjBybHkza2xibjJpY3NrbjEifQ.jph0u9y6XM4ySmoi0J-eGQ';
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoia3VuYWxndWxhdGkwODE4IiwiYSI6ImNsMngyZWlxNjBybHkza2xibjJpY3NrbjEifQ.jph0u9y6XM4ySmoi0J-eGQ";
 
 const ConfirmPage = () => {
-    const map = useRef();
-    const mapContainer = useRef(null);
-    const router = useRouter();
-    const [lng, setLng] = useState(77.2610);
-	const [lat, setLat] = useState(28.5354);
-	const [zoom, setZoom] = useState(9);
+  const map = useRef();
+  const mapContainer = useRef(null);
+  const router = useRouter();
+  const [lng, setLng] = useState(77.261);
+  const [lat, setLat] = useState(28.5354);
+  const [zoom, setZoom] = useState(9);
 
-    useEffect(() => {
-        const pickup = router?.query?.pickup;
-        const destination = router?.query?.destination;
+  useEffect(() => {
+    const pickup = router?.query?.pickup;
+    const destination = router?.query?.destination;
 
-        if(!pickup || !destination) {
-            return
-        }
-        const fetchData = async () => {
-            let data = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${pickup};${destination}.json?access_token=pk.eyJ1Ijoia3VuYWxndWxhdGkwODE4IiwiYSI6ImNsMngyZWlxNjBybHkza2xibjJpY3NrbjEifQ.jph0u9y6XM4ySmoi0J-eGQ`)
-            return data;
-        }
+    if (!pickup || !destination) {
+      return;
+    }
 
-        const data = fetchData();
+    if (map.current) return;
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/dark-v10",
+      center: [lng, lat],
+      zoom: zoom,
+    });
 
-        // console.log('data', data)
-        // return
-        if(map.current) return;
-        map.current = new mapboxgl.Map({
-			container: mapContainer.current,
-			style: 'mapbox://styles/mapbox/dark-v10',
-			center: [
-				lng, lat
-			],
-			zoom: zoom
-		});
-    }, [router.query]);
-    return (
-        <ConfirmPageWrapper>
-            <MapWrapper ref = {mapContainer} id="confirm-page-map-wrapper">
-                Hello
-            </MapWrapper>
-            <ShowRideWrapper>
-                <MessageWrapper>
-                    Choose a ride, or swipe up for more
-                </MessageWrapper>
-            </ShowRideWrapper>
-            <FooterWrapper>
-                <ConfirmButton>
-                    Confirm uber
-                </ConfirmButton>
-            </FooterWrapper>
-        </ConfirmPageWrapper>
-    )
-}
+    const fetchData = async () => {
+        let pickupData = await axios.get(
+          `https://api.mapbox.com/geocoding/v5/mapbox.places/${pickup}.json?access_token=pk.eyJ1Ijoia3VuYWxndWxhdGkwODE4IiwiYSI6ImNsMngyZWlxNjBybHkza2xibjJpY3NrbjEifQ.jph0u9y6XM4ySmoi0J-eGQ&limit=1`
+        );
+          console.log('pic', pickupData)
+        let destinationData = await axios.get(
+          `https://api.mapbox.com/geocoding/v5/mapbox.places/${destination}.json?access_token=pk.eyJ1Ijoia3VuYWxndWxhdGkwODE4IiwiYSI6ImNsMngyZWlxNjBybHkza2xibjJpY3NrbjEifQ.jph0u9y6XM4ySmoi0J-eGQ&limit=1`
+        );
+        addToMap({ pickupData: pickupData?.data?.features?.length ?  pickupData?.data?.features[0]?.center : [], destinationData: destinationData?.data?.features?.length ? destinationData?.data?.features[0]?.center : [] });
+      };
+
+      fetchData();
+  }, [router.query]);
+
+  const addToMap = ({ pickupData, destinationData }) => {
+    const marker1 = new mapboxgl.Marker({
+        color: "green",
+        }).setLngLat(pickupData)
+        .addTo(map.current);
+
+    const marker2 = new mapboxgl.Marker({
+            color: "red",
+            }).setLngLat(destinationData)
+            .addTo(map.current);
+  }
+  return (
+    <ConfirmPageWrapper>
+      <MapWrapper ref={mapContainer} id="confirm-page-map-wrapper"></MapWrapper>
+      <ShowRideWrapper>
+        <MessageWrapper>Choose a ride, or swipe up for more</MessageWrapper>
+        <CarRideWrapper>
+          {carList.map((car) => (
+            <CarOptionsWrapper>
+              <CarWrapper>
+                <CarImage src={car.imgUrl} />
+                <CarLabelWrapper>
+                  <CarLabel>{car.service}</CarLabel>
+                  <CarTimeWrapper>5 min away</CarTimeWrapper>
+                </CarLabelWrapper>
+              </CarWrapper>
+              <PriceWrapper>${car.multiplier}</PriceWrapper>
+            </CarOptionsWrapper>
+          ))}
+        </CarRideWrapper>
+      </ShowRideWrapper>
+      <FooterWrapper>
+        <ConfirmButton>Confirm uber</ConfirmButton>
+      </FooterWrapper>
+    </ConfirmPageWrapper>
+  );
+};
 
 const ConfirmPageWrapper = tw.div`
     h-screen flex flex-col
@@ -74,17 +99,47 @@ const ShowRideWrapper = tw.div`
 `;
 
 const MessageWrapper = tw.div`
-    border-b-2 border-slate-200 flex items-center justify-center text-sm text-slate-500
+    border-b-2 border-slate-200 flex items-center justify-center text-sm text-slate-500 p-1
 `;
 
-const CarOptionsWrapper = tw.div``
+const CarRideWrapper = tw.div`
+    flex flex-col p-4 gap-y-8 max-h-[80%] overflow-y-auto
+`;
+
+const CarOptionsWrapper = tw.div`
+    flex justify-between items-center
+`;
+
+const CarImage = tw.img`
+    flex items-center gap-x-2 h-12 w-12
+`;
+
+const CarWrapper = tw.div`
+    flex items-center gap-x-2
+`;
+
+const CarLabelWrapper = tw.div`
+flex flex-col
+`;
+
+const CarLabel = tw.div`
+text-sm font-medium
+`;
+
+const CarTimeWrapper = tw.div`
+text-xs text-sky-700
+`;
+
+const PriceWrapper = tw.div`
+    text-sm
+`;
 
 const FooterWrapper = tw.div`
-p-2.5 bg-white border-t-2 border-slate-200
+absolute bottom-0 w-full p-2.5 bg-white border-t-2 border-slate-200
 `;
 
 const ConfirmButton = tw.button`
 bg-black text-white text-xl w-full p-3 flex items-center justify-center
 `;
 
-export default ConfirmPage
+export default ConfirmPage;
